3930c738e1568885f5750cfdd588631c269eb74d
carlcarl <carlcarlking@gmail.com>
2013-06-19 16:56:28 +0800

    Update version to 1.2.0

--- setup.py
+++ setup.py
@@ -3,9 +3,9 @@ from setuptools import find_packages
 
 setup(
     name='imgurup',
-    description='Upload to imgur using API(v3). Support CLI, KDE and Mac dialog upload. And you can also use your account to upload :).',
+    description='Upload to imgur using API(v3). Support CLI, KDE,Zenity(GTK) and Mac dialog upload. And you can also use your account to upload :).',
     long_description=open('README.rst').read(),
-    version='1.1.0',
+    version='1.2.0',
     author='carlcarl',
     author_email='carlcarlking@gmail.com',
     url='https://github.com/carlcarl/imgurup',
