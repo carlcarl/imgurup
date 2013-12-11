150a110575c17190d7669546baa434696a160f69
carlcarl <carlcarlking@gmail.com>
2013-12-12 00:19:58 +0800

    Update to 1.5.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     name='imgurup',
     description='Upload to imgur using API(v3). Support CLI, KDE,Zenity(GTK) and Mac dialog upload. And you can also use your account to upload :).',
     long_description=open('README.rst').read(),
-    version='1.5.1',
+    version='1.5.2',
     author='carlcarl',
     author_email='carlcarlking@gmail.com',
     url='https://github.com/carlcarl/imgurup',
